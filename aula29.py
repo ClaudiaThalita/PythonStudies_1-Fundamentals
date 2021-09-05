df32d57743bb89685b7e9e78e8d46204be39d266
ClaudiaThalita <claudiathalitamendes@outlook.com>
2021-09-05 17:50:44 -0300

    feature: aula29 -Operadores aritimeticos

---
+++ aula29.py
@@ -0,0 +1,10 @@
+print( 2 + 3)
+print (4 - 7 )
+print (2 *5.2)
+print (9.4 // 3 ) # obriga o valor final ser inteiro, apesar do resultado ser float
+print( 2** 8) #exponenciação
+print(10 % 3) # resto
+
+a =12
+b = a
+print (a + b)
