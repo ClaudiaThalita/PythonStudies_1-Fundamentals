e75ba7ec832c7d6a0847c7c42cb4da038d07bb0f
ClaudiaThalita <claudiathalitamendes@outlook.com>
2021-09-05 17:42:17 -0300

    feature: aula28

---
+++ aula28.py
@@ -0,0 +1,18 @@
+# minhas variaveis
+salario = 3450.45
+despesas = 2456.2
+
+#comentar
+#varias
+# por linha
+
+
+"""a ideia é quanto vai
+ sobrar no final do 
+ mês
+"""
+
+print(salario - despesas)
+
+print('Fim de verdade')
+ 
